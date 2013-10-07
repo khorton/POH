979afd57552e60056e6f6da327c2cbffd9b76bfe
Kevin Horton <kevin01@kilohotel.com>
2013-10-06 20:37:48 -0400

    New date.  Editorial clean up in climb perf.

--- latex/source_files/perf_climb_FF.py
+++ latex/source_files/perf_climb_FF.py
@@ -5,5 +5,5 @@ import std_atm as SA
 FF0 = 17
 for alt in range(0, 20001, 2000):
     sigma = SA.alt2density_ratio(alt)
-    FF = sigma * FF0
-    print "%.0f %.0f" % (alt, FF)
+    FF = sigma * FF0 * 2700/2700
+    print "%.0f %.1f" % (alt, FF)
